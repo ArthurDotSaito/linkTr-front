c32c0ab12ca88fd885dcc518cc6e84f7819aa602
Gui-byte-dot <grgomes82@gmail.com>
2023-03-08 22:24:11 -0300

    posts

--- src/Timeline.js
+++ src/Timeline.js
@@ -53,32 +53,160 @@ export default function Timeline() {
             <Title>Linkr</Title>
             <Second>timeline</Second>
             <PublishPost>
-                <p>What are you going to share today?</p>
-                <InputUrl type="text" placeholder="http:// ..." value={url} onChange={(e) => setUrl(e.target.value)}></InputUrl>
-                <InputDescription type="text" placeholder="Awesome article about #javascript" value={description} onChange={(e) => setDescription(e.target.value)}></InputDescription>
-                <Publish onClick={Postar}>Publish</Publish>
+                <img src="https://www.lance.com.br/files/article1x1/uploads/2023/03/07/6407e30e59947.jpeg" />
+                <div>
+                    <p>What are you going to share today?</p>
+                    <InputUrl type="text" placeholder="http:// ..." value={url} onChange={(e) => setUrl(e.target.value)}></InputUrl>
+                    <InputDescription type="text" placeholder="Awesome article about #javascript" value={description} onChange={(e) => setDescription(e.target.value)}></InputDescription>
+                    <Publish onClick={Postar}>Publish</Publish>
+                </div>
             </PublishPost>
-            <GetPosts>
                 {posts.map((post,index) => 
-                <><div key={index}>
-                        <img src={post.image} />
-                        <section>
-                            <PostName>{post.name}</PostName>
-                            <PostDescription>{post.description}</PostDescription>
-                        </section>
+                <UserPost key={index}>
+                    <ImageName>
+                        <ImageUser src={post.image}/>
+                        <InfoUser>
+                            <p>{post.name}</p>
+                            <p>{post.description}</p>
+                        </InfoUser>
+                    </ImageName>
+                    <ImageUrl>
+                        <Urls>
+                            <p>{post.titleUrl}</p><p>{post.descriptionUrl}</p><p>{post.url}</p> 
+                        </Urls>
+                        <img src={post.imageUrl} />
 
 
-                    </div><p>{post.titleUrl}</p><p>{post.descriptionUrl}</p><ImagePage src={post.imageUrl} /></>
+                    </ImageUrl>
+                     
+                </UserPost>
                 
 
                 
                 )}
-            </GetPosts>
 
         </>
 
     );
 }
+const Urls = styled.div`
+    display:flex;
+    flex-direction:column;
+    padding-top:24px;
+    p:nth-child(1){
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 19px;
+        color: #CECECE;
+    }
+    p:nth-child(2){
+        padding-top:5px;
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 11px;
+        line-height: 13px;
+        color: #9B9595;
+    }
+    p:nth-child(3){
+        padding-top:13px;
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 11px;
+        line-height: 13px;
+        color: #CECECE;
+    }
+`
+
+const ImageUrl = styled.div`
+    display:flex;
+    flex-direction:row;
+    img{
+        width: 153.44px;
+        height: 155px;
+    }
+    box-sizing: border-box;
+    width: 503px;
+    height: 155px;
+    left: 502px;
+    top: 596px;
+    margin-left:87px;
+    margin-top:10px;
+
+    border: 1px solid #4D4D4D;
+    border-radius: 11px;
+`
+
+const UserPost = styled.div`
+    display:flex;
+    flex-direction:column;
+    margin-left:415px;
+width: 611px;
+height: 276px;
+left: 415px;
+top: 495px;
+
+background: #171717;
+border-radius: 16px;
+margin-top:20px;
+`
+const ImageUser = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 26.5px;
+`
+const ImageName = styled.div`
+    display:flex;
+    flex-direction:row;
+    padding-left:18px;
+    padding-top:17px;
+`
+
+const InfoUser = styled.div`
+    display:flex;
+    flex-direction:column;
+    margin-left:18px;
+    p:nth-child(1){
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19px;
+        line-height: 23px;
+        color: #FFFFFF;
+    }
+    p:nth-child(2){
+        font-family: 'Lato';
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17px;
+        line-height: 20px;
+        color: #B7B7B7;
+    }
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const Header = styled.div`
     position: absolute;
@@ -111,7 +239,7 @@ const Title = styled.div`
 const PublishPost = styled.div`
     position: relative;
     display:flex;
-    flex-direction:column;
+    flex-direction:row;
     align-items:center;
     justify-content:center;
     margin-left:415px;
@@ -127,6 +255,28 @@ const PublishPost = styled.div`
         color:#707070
 
     }
+    div{
+        display:flex;
+        flex-direction:column;
+        margin-left:18px;
+    }
+    img{
+        width: 50px;
+        height: 50px;
+        margin-bottom:100px;
+        border-radius: 26.5px;
+        margin-left:16px;
+
+    }
+    div p{
+            font-family: 'Lato';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 24px;
+
+    color: #707070;
+    }
 
  `;
  const InputUrl = styled.input`
@@ -137,15 +287,44 @@ const PublishPost = styled.div`
     background: #EFEFEF;
     border-radius: 5px;
     margin-top:10px;
+    border:none;
+    ::placeholder{
+        padding-left:12px;
+        font-family: 'Lato';
+font-style: normal;
+font-weight: 300;
+font-size: 15px;
+line-height: 18px;
+/* identical to box height */
+
+
+color: #949494;
+
+    }
  `;
  const InputDescription = styled.input`
- width: 502px;
- height: 66px;
- left: 501px;
- top: 313px;
- background: #EFEFEF;
- border-radius: 5px;
- margin-top:5px;
+    width: 502px;
+    height: 66px;
+    left: 501px;
+    top: 313px;
+    background: #EFEFEF;
+    border-radius: 5px;
+    margin-top:5px;
+    border:none;
+    ::placeholder{
+        padding-left:12px;
+        font-family: 'Lato';
+font-style: normal;
+font-weight: 300;
+font-size: 15px;
+line-height: 18px;
+/* identical to box height */
+
+
+color: #949494;
+padding-top:8px;
+
+}
 
  `
  const Publish = styled.button`
@@ -157,8 +336,16 @@ top: 419px;
 
 background: #1877F2;
 border-radius: 5px;
+font-family: 'Lato';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 17px;
+/* identical to box height */
 
- `
+
+color: #FFFFFF;
+margin-left:390px `
 const Second = styled.p`
     width: 145px;
     height: 64px;
@@ -174,60 +361,59 @@ const Second = styled.p`
     margin-left:415px;
 
  `
- const GetPosts = styled.div`
-    display:flex;
-    margin-top:29px;
-    flex-direction:column;
-    margin-left:415px;
+//  const GetPosts = styled.div`
+//     display:flex;
+//     margin-top:29px;
+//     flex-direction:column;
+//     margin-left:415px;
 
-    div{
-        margin-top:16px;
-        display:flex;
-        flex-direction:row;
-        width: 611px;
-        height: 276px;
-        background: #171717;
-        border-radius: 16px;
-        padding-left:18px;
-        padding-top:18px;
-    }
-    div img{
-        width: 50px;
-        height: 50px;
-        border-radius: 26.5px;
+//     div{
+//         margin-top:16px;
+//         display:flex;
+//         flex-direction:colum;
+//         width: 611px;
+//         height: 276px;
+//         border-radius: 16px;
+//         padding-left:18px;
+//         padding-top:18px;
+//     }
+//     div img{
+//         width: 50px;
+//         height: 50px;
+//         border-radius: 26.5px;
 
 
-    }
-    div section{
-        flex-direction:column;
-        margin-left:18px;
-    }
- `
- const PostName = styled.p`
-    font-weight: 400;
-    font-size: 19px;
-    line-height: 23px;
-    color: #FFFFFF;
- `
- const PostDescription = styled.p`
-    font-style: normal;
-    font-weight: 400;
-    font-size: 17px;
-    line-height: 20px;
-    color: #B7B7B7;
-`
-const ImageUrl = styled.div`
-width: 153.44px;
-height: 155px;
-left: 851.56px;
-top: 596px;
-`
+//     }
+//     div section{
+//         flex-direction:column;
+//         margin-left:18px;
+//     }
+//  `
+//  const PostName = styled.p`
+//     font-weight: 400;
+//     font-size: 19px;
+//     line-height: 23px;
+//     color: #FFFFFF;
+//  `
+//  const PostDescription = styled.p`
+//     font-style: normal;
+//     font-weight: 400;
+//     font-size: 17px;
+//     line-height: 20px;
+//     color: #B7B7B7;
+// `
+// const ImageUrl = styled.div`
+// width: 153.44px;
+// height: 155px;
+// left: 851.56px;
+// top: 596px;
+// `
 
-const ImagePage = styled.img`
-    width: 153.44px;
-    height: 155px;
-`
-const AllUrl = styled.div`
+// const ImagePage = styled.img`
+//     width: 153.44px;
+//     height: 155px;
+// `
+// const AllUrl = styled.div`
     
-`
+// `
 
