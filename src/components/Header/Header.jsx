9acbbe311536e0efaf820477fe9e2e5ae9d8fa61
Gui-byte-dot <grgomes82@gmail.com>
2023-03-19 00:23:02 -0300

    posts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -156,3 +156,76 @@ export default function Header() {
 }
 
 
+
+const SearchBar = styled(DebounceInput)`
+  width: 100%;
+  height: 3rem;
+  ::placeholder {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    line-height: 22.8px;
+    color: #c6c6c6;
+    padding: 1rem;
+  }
+`;
+
+const ResultsContainer = styled.div`
+  background-color: #e7e7e7;
+  border-radius: 0 0 5px 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  max-height: 250px;
+  position: absolute;
+  top: 3.7rem;
+  width: 33%;
+`;
+
+const NoResults = styled.div`
+  color: #999;
+`;
+
+const Result = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem 0;
+`;
+
+const Avatar = styled.img`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+`;
+
+const Username = styled.h3`
+  height: 2rem;
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+  line-height: 22.8px;
+  color: #515151;
+`;
+
+const LogoutMenu = styled.div`
+  width: 150px;
+  height: 43px;
+  background-color: #171717;
+  border-radius: 0px 0px 20px 20px;
+  position: absolute;
+  top: 80px;
+  right: 0px;
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Lato";
+    font-size: 15px;
+    line-height: 18px;
+    letter-spacing: 0.05em;
+    color: #ffffff;
+    padding-right: 10px;
+  }
+  display: ${(props) => (!props.turn ? "flex" : "none")};
+  :hover {
+    cursor: pointer;
+  }
+`;
